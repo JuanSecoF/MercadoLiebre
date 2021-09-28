fedba7bc79dc05e8ef1894d6c2da45b76cf6f23d
juan seco <juansecof97@gmail.com>
2021-09-28 19:08:28 -0300

    subo mercadoliebre

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const path = require("path")
+const app= express()
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.listen('3000', () => {
+    console.log(
+        'Bien ahi genio iniciaste en el servidor',
+        'http://localhost:3000'
+    )
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+})
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+app.post('/register' , (req,res) =>{
+    
+    res.redirect('/')
+})
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+app.post('/login' , (req,res) =>{
+    res.redirect('/')
+})
